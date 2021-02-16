3637c33e37f905a46335ba0fc8902d8984fe9724
Chris Ahrens <christopher00ahrens@gmail.com>
2021-02-15 23:50:09 -0800

    Initial Commit

---
+++ AgrecomApp/AgrecomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgrecomAppApp.swift
+//  AgrecomApp
+//
+//  Created by Chris Ahrens on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct AgrecomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
