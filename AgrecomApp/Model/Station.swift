e78584c830186191a94e8f9521b4f7e12d4e6739
Chris Ahrens <christopher00ahrens@gmail.com>
2021-04-23 23:36:25 -0700

    station dummy data

--- AgrecomApp/Model/Station.swift
+++ AgrecomApp/Model/Station.swift
@@ -6,10 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Station: Hashable, Codable, Identifiable {
-    var id: UUID
-    var siteId: Int
+    var id = UUID()
+    var siteID: Int
     var stationID: Int
     
+}
+
+extension Station {
+    static func dummyData() -> [Station] {
+        var array = [Station]()
+        
+        for i in 0...30{
+            let stat = Station(siteID: 10001, stationID: (10000+i))
+            array.append(stat)
+        }
+        
+        return array;
+    }
 }
