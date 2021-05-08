24f9ba56793e1d95b255fa6ce5713413217756e3
chrisachiles0 <56chris10@gmail.com>
2021-05-08 16:57:37 -0700

    began new implementation

---
+++ AgrecomApp/Helpers/dbInterface.swift
@@ -0,0 +1,18 @@
+//
+//  dbInterface.swift
+//  AgrecomApp
+//
+//  Created by Chris Achiles and John Villalta on 4/28/21.
+//
+
+import Foundation
+import PostgresKit
+import PostgresNIO
+/*
+ /* Attempts a connection with authentication to the database, and afterwards
+    performs a basic querying test of the database
+ */
+ func connect() {
+     
+ }
+*/
