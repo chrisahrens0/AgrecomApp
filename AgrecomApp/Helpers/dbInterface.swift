b9fb54f450a57990e361691e23d6c8891bc47e9f
chrisachiles0 <56chris10@gmail.com>
2021-05-08 17:12:28 -0700

    initial framework for querying and database retrieval provided

--- AgrecomApp/Helpers/dbInterface.swift
+++ AgrecomApp/Helpers/dbInterface.swift
@@ -23,11 +23,31 @@ import PostgresNIO
     defer { try! conn.close().wait() }
 
     //authenticate the connection with the database
-    try conn.authenticate(
+    try! conn.authenticate(
         username: "vapor_username",
         database: "vapor_database",
         password: "vapor_password"
     ).wait()
 
+    //database and initial query testing
+    let db: PostgresDatabase;
+
+    //print a row of the Azure database to test
+    //perform a parameterized query
+    let rows = try! db.query("SELECT * FROM planets WHERE name = $1", ["Earth"]).wait()
+    print(rows) // [["id": 42, "name": "Earth"]]
+
+    //if necessary, print any columns as well
+    let row: PostgresRow;
+    let version = row.column("version")
+    print(version) // PostgresData?
+
+    //create a PostgresData type to convert column data to compatible Swift types
+    let data: PostgresData;
+
+    //after querying, print the data retreived in an appropriate format
+    //print(try! data.jsonb(as: Foo.self)) //json example, Foo to be replaced
+    //print(data.string)
+    //print(data.int)
  }
 */
